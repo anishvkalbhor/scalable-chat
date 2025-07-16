2f6216e1a6b16e2233841295fc8b6f0f7e78921b
Anish Kalbhor <anishkalbhor2020@gmail.com>
2025-07-17 01:14:50 +0530

    Scalable chat application using redis cloud server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,17 +1,19 @@
-import http from 'http';
-import SocketService from './services/socket';
+import http from "http";
+import SocketService from "./services/socket";
 
 async function init() {
-    const socketService = new SocketService();
-    
-    const httpServer = http.createServer();
-    const PORT = process.env.PORT ? process.env.PORT : 8000;
+  const socketService = new SocketService();
 
-    socketService.io.attach(httpServer);
+  const httpServer = http.createServer();
+  const PORT = process.env.PORT ? process.env.PORT : 8000;
 
-    httpServer.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  socketService.io.attach(httpServer);
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  socketService.initListeners();
 }
 
 init();
